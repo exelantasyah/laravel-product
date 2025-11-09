a2daa007413714b0db5cb667406502228ade3362
exelantasyah <exelantasyah08@gmail.com>
2025-11-09 07:52:15 +0700

    Menentukan Rute Sumber Daya Peran, Pengguna, dan Produk

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +12,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::resources([
+    'roles' => RoleController::class,
+    'users' => UserController::class,
+    'products' => ProductController::class,
+]);
